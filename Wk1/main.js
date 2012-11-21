929232765e6a655eba1012b9eae54c17c6eabf85
Bodunrin Ladele <bladele@me.com>
2012-11-20 23:33:52 -0500

    commit before midnight.

--- Wk1/main.js
+++ Wk1/main.js
@@ -4,3 +4,32 @@
 $('#home').on('pageinit', function(){
     //code needed for home page goes here
 });
+
+$('#additem').on('pageinit', function(){
+	
+	var iForm = $('#additemform'),
+		errorslink = $('#errorslink');
+
+		iForm.validate({
+		invalidHandler: function(form, validator) {
+			errorslink.click();
+			var html = '';
+			$("#errors ul").html("");
+			for(var key in validator.submitted){
+				var label = $('lable[for^="'+ key +'"]').not('[generated]');
+				var legend = label.closet('fieldset').find('.ui-controlgroup-label');
+				var fieldName = legend.lenght ? legend.text() : label.text();
+				html += '<li>' + fieldName + '</li';
+			};
+			$("#errors ul"). html(html);
+		},
+		submitHandler: function() {
+			var data = iForm.serializeArray();
+			console.log(data);
+			storeData(data);
+		}
+	});
+		
+
+});
+
